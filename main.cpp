fd70db1f95dc87c6a4dcc5905255b013cab70bdc
lorenzocabirta7 <lcabirta@fi.uba.ar>
2023-12-07 11:05:58 -0300

    CAMBIOS EN MAIN
    Co-Authored-By: Roman Frattasi <133982658+romanfrattasi@users.noreply.github.com>
    Co-Authored-By: juanbalella20 <133823704+juanbalella20@users.noreply.github.com>
    Co-Authored-By: Beltran Malbran <113385188+BeltranMal@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,23 +1,9 @@
-/*
-#include "ABB.hpp"
-#include "Placa.hpp"
-#include "bGVjdG9y.hpp"
-#include <fstream>
-*/
-#include "include/Inventario.hpp"
-//#include <iostream>
-//#include "Interfaz.hpp"
+
 #include "Menu.hpp"
-//#include "Personaje.hpp"
-
-#include <vector>
-#include "Grafo.hpp"
-
 using namespace std;
 
 int main()
 {
-    Inventario inventario;
     srand((unsigned)time(NULL));
     Menu menu;
     menu.flujo_juego();
